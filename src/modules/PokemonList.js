76996ae9771e598f246b4c2abc11cae8d8b695c4
velezmidi <velez.midi@gmail.com>
2021-06-18 17:44:52 -0500

    reducer setting reducer

--- src/modules/PokemonList.js
+++ src/modules/PokemonList.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PokemonList() {
+  const reducer = (state, action) => {};
+  const [state, dispatch] = useState(reducer);
   return <div>NOC</div>;
 }
 
