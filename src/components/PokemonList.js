ccee6c1dd482ebbe375f36cf3a4ebdde6bcd0565
Velezmidi <velez.midi@gmail.com>
2021-06-23 16:37:51 -0500

    Added funtionality it now fetches information form the api and renders it

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,19 +1,61 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useFetch } from "../customHooks/useFetch";
 
 function PokemonList() {
-  const [pokemons, setPokemons] = useState([]);
-  const [previousUrl, setPreviousUrl] = useState("");
-  const [nextUrl, setNextUrl] = useState("");
   const [currentUrl, setCurrentUrl] = useState(
     "https://pokeapi.co/api/v2/pokemon"
   );
 
   const { isLoading, data } = useFetch(currentUrl);
+  const { next, previous, results } = data;
 
-  console.log(data);
+  const [pokemons, setPokemons] = useState([]);
+  const [previousUrl, setPreviousUrl] = useState("");
+  const [nextUrl, setNextUrl] = useState("");
 
-  return <div>Prueba</div>;
+  useEffect(() => {
+    setPokemons(results);
+    setPreviousUrl(previous);
+    setNextUrl(next);
+  }, [data]);
+
+  const handleNext = () => {
+    setCurrentUrl(nextUrl);
+  };
+  const handlePrev = () => {
+    setCurrentUrl(previousUrl);
+  };
+
+  if (isLoading) {
+    return (
+      <h1>
+        Loading... <div>dk</div>
+      </h1>
+    );
+  } else {
+    return (
+      <>
+        {pokemons.map((element) => {
+          const { name, url } = element;
+          return (
+            <div key={name}>
+              <h2>{name}</h2>
+            </div>
+          );
+        })}
+        {previousUrl && (
+          <button type="button" className="btn" onClick={handlePrev}>
+            Previous
+          </button>
+        )}
+        {nextUrl && (
+          <button type="button" className="btn" onClick={handleNext}>
+            Next
+          </button>
+        )}
+      </>
+    );
+  }
 }
 
 export default PokemonList;
