b69ae8b88d3e7a090869c9f30705b4c7f676df03
Velezmidi <velez.midi@gmail.com>
2021-06-26 01:59:14 -0500

    setting upp stuff becouse i cannot sleep

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -8,10 +8,16 @@ function PokemonPage() {
   const { handle } = useParams();
   const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${handle}`;
   const { isLoading, data, isError } = useFetch(pokemonUrl);
-  console.log(data);
+  // console.log(data);
+  const { id } = data;
+  console.log(id);
 
   if (!isError) {
-    return <div>Wololo {handle}</div>;
+    return (
+      <div>
+        Wololo {handle} {id}
+      </div>
+    );
   } else {
     return <NotFoundPok />;
   }
